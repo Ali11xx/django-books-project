02704b7647e0e40f9f27bf38d6f158d364710640
Mohamed S. Omar <alitarek265@gmail.com>
2023-11-18 15:48:23 +0200

    update the api

--- book/views.py
+++ book/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .serializers import AuthorSerializer, BookSerializer, ReviewSerializer
+from .models import Author, Book
+
 # Create your views here.
+class AuthorListAPI(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookListAPI(generics.ListCreateAPIView): 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailAPI(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
